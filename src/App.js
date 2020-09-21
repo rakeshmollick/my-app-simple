0b7997fd19b3762a19f6ed04e78a8cdcc1f01ca0
rakeshmollick <rakesmallik@gmail.com>
2020-09-21 12:22:18 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+ 
 function App() {
+const [nayoks, setNayoks] = useState([])
+console.log(count)
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setNayoks(data))
+  }, [])
   return (
     <div className="App">
+      <MovieCounter>
+        {
+          nayoks.map(nk => <Nayok name = {nk.name} age = {nk.age}></Nayok>)
+        }
+      </MovieCounter>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -16,11 +28,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React from Scratch
         </a>
       </header>
     </div>
   );
 }
-
+ 
 export default App;
